fb8ce99f048907288b73042fde7785e69315b8af
HuanCheng Bai <best.tony@foxmail.com>
2017-12-28 23:28:47 +0800

    fixed URL route

--- bin/cli.js
+++ bin/cli.js
@@ -2,7 +2,7 @@
 var program = require("commander");
 var colors = require('colors');
 var turnoff = require('../lib/turnoffus')
-
+var monkeyuser = require("../lib/monkeyusercom");
 // 输出版本号和说明 
 program
   .version('0.0.1');
@@ -24,8 +24,16 @@ program
   .description("Use to deal a url") //定义描述
   .action(function(url){
      // 根据传入的 URL 判断使用哪个网站。
-     // todo： 后续考虑将站点文件移动到一个单独的目录下，这样可以在这里通过读取文件列表来实现
-     turnoff.main(url);
+     // @todo： 后续考虑将站点文件移动到一个单独的目录下，这样可以在这里通过读取文件列表来实现
+
+     // @todo: 后续考虑以更加优雅的实现来进行分发
+     if(url.indexOf("turnoff.us") != -1){
+        turnoff.main(url);
+     }else{
+       if (url.indexOf("monkeyuser") != -1){
+        monkeyuser.main(url);
+       }
+     }
   });
 
 // 解析参数
